61e986147a7e1754d9b078feb71cc492510febfa
Florent Jaby <fjaby@octo.com>
2014-12-19 13:10:40 +0100

    use fun.name

--- paperwork.js
+++ paperwork.js
@@ -118,12 +118,7 @@ function paperwork(spec, val, visitor) {
 }
 
 function getFunctionName(fun) {
-  var ret = fun.toString();
-
-  ret = ret.substr('function '.length);
-  ret = ret.substr(0, ret.indexOf('('));
-
-  return ret || 'custom validator';
+  return fun.name || 'custom validator';
 }
 
 module.exports = function (spec, blob, done) {
